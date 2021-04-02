22fa312c13696df81c064eea068ddc0ee310ed89
yungo1846 <yungo1468@naver.com>
2021-04-02 22:20:50 +0900

    deploy

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
-// import '../css/index.css';
+import '../css/index.css';
 import App from './components/App.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   new App();
-  console.log(window.location.pathname);
-  const popStateEvent = new Event('popstate');
-  window.dispatchEvent(popStateEvent);
 });
